2d42475c19e91d7b46d13c4e04abddcaf494cf63
jaidtanwar <jaidtanwar@deloitte.com>
2022-07-19 14:41:31 +0530

    minor changes

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using PuppeteerSharp;
-using System.Reflection.Metadata;
+using Aspose.Pdf;
+using System.Collections.Generic;
 
 namespace FunctionApp1
 {
@@ -34,24 +35,27 @@ namespace FunctionApp1
             using (var browser = await Puppeteer.LaunchAsync(options))
             using (var page = await browser.NewPageAsync())
             {
-                await page.GoToAsync("http://www.facebook.com");
-
+                await page.GoToAsync("http://www.google.com");
+                //await page.WaitForSelectorAsync("img.lnXdpd");
+                //var watchDog = await page.WaitForFunctionAsync(" () => window.innerWidth < 100");
+                //await page.SetViewportAsync(new ViewPortOptions { Width = 50, Height = 50 });
                 Console.WriteLine("Generating PDF");
+
+                //Generating PDF
                 await page.PdfAsync(Path.Combine("C:\\Users\\jaidtanwar\\source\\repos\\FunctionApp1\\FunctionApp1\\", "facebook.pdf"));
-
-
                 Console.WriteLine("Export completed");
 
+                //Convert to PDF to PPT
+                Document pdfDocument = new Document(Path.Combine("C:\\Users\\jaidtanwar\\source\\repos\\FunctionApp1\\FunctionApp1\\", "facebook.pdf"));
+                PptxSaveOptions pptxOptions = new PptxSaveOptions();
+                // Save output file
+                pdfDocument.Save(Path.Combine("C:\\Users\\jaidtanwar\\source\\repos\\FunctionApp1\\FunctionApp1\\", "facebook.pptx"), pptxOptions);
 
-                //Console.ReadLine();
-                //if (!args.Any(arg => arg == "auto-exit"))
-                //{
-                //    Console.ReadLine();
-                //}
+
             }
 
 
-            string responseMessage = "Downloaded PDF";
+            string responseMessage = "Downloaded PDF and PPT";
 
             return new OkObjectResult(responseMessage);
         }
